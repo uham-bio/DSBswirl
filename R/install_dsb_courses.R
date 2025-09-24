8e53ef0fb068de5c1b80b166d0e187780e8a45b6
saskiaotto <saskia.otto@uni-hamburg.de>
2025-09-24 20:26:28 +0200

    made small changes to the R function documentation

--- R/install_dsb_courses.R
+++ R/install_dsb_courses.R
@@ -2,35 +2,42 @@
 #'
 #' Install all or a selection of swirl courses developed for the DSB (Data Science in
 #' Biology) program within the biology department of the University of Hamburg (UHH).
-#' The package comes currently with six swirl courses that can be installed
+#' The package currently includes seven swirl courses that can be installed
 #' using this \code{install_dsb_courses} function.
 #'
 #' @param courses character; single string or vector with the name of the courses to be
-#' installed. By default, all courses will be installed but you can also select from the
-#' the following courses: 'DSB-01', 'DSB-02', 'DSB-03', 'DSB-04', 'DSB-05', 'DSB-06'.
-#' See under details what topics each course covers.
+#' installed. The default setting is 'all', which installs all German courses, but you
+#' can also select from the following courses:
+#' 'DSB-01', 'DSB-02', 'DSB-03', 'DSB-04', 'DSB-05', 'DSB-06', 'Data analysis with R'.
+#' See \emph{Details} for the topics each course covers.
 #' @param force logical; should course installation be forced if the course is already
-#'   installed? The default value is FALSE.
+#'   installed? The default value is \code{FALSE}.
 #'
 #' @details
 #' The following swirl courses can be installed (currently only in German):
 #' \describe{
-#'   \item{DSB-01}{Basics in R (R Grundlagen)}
-#'   \item{DSB-02}{Data exploration with R (Datenexploration mit R)}
-#'   \item{DSB-03}{Data warangling and introduction to tidyverse (Datenaufbereitung
-#'     oder per Anleitung durchs Tidyversum)}
-#'   \item{DSB-04}{Data visualization with ggplot2 (Datenvisualisierung mit ggplot2)}
-#'   \item{DSB-05}{Handling of special data types (Handling spezieller Datentypen)}
-#'   \item{DSB-06}{Advanced R Programming (Fortgeschrittene R Programmierung)}
+#'   \item{DSB-01}{R Grundlagen (Basics in R)}
+#'   \item{DSB-02}{Datenexploration mit R (Data exploration in R))}
+#'   \item{DSB-03}{Datenaufbereitung oder per Anleitung durchs Tidyversum
+#'                 (Data wrangling and introduction to tidyverse)}
+#'   \item{DSB-04}{Datenvisualisierung mit ggplot2 (Data visualization with ggplot2)}
+#'   \item{DSB-05}{Handling spezieller Datentypen (Working with special data types)}
+#'   \item{DSB-06}{Fortgeschrittene R Programmierung (Advanced R programming)}
+#' }
+#' The following swirl course is in English:
+#' \describe{
+#'   \item{Data analysis with R}{From R basics to visualization and statistical modelling}
 #'  }
 #'
 #' @examples
 #' \dontrun{
-#'  # Install all courses (default)
+#'  # Install all German courses (default)
 #'  install_dsb_courses()
-#'  # Install only course DSB-01 (R basics)
+#'
+#'  # Install only the DSB-01 (R basics) course
 #'  install_dsb_courses(courses = "DSB-01")
-#'  # Install the course DSB-01, DSB-04, and DSB-05
+#'
+#'  # Install the courses DSB-01, DSB-04, and DSB-05
 #'  install_dsb_courses(courses = c("DSB-01", "DSB-04", "DSB-05"))
 #' }
 #' @export
@@ -40,7 +47,7 @@ install_dsb_courses <- function(courses = "all", force = FALSE) {
   current_courses <- c("DSB-01", "DSB-02", "DSB-03", "DSB-04", "DSB-05", "DSB-06", "Data analysis with R")
   # Validate course selection
   if (!all(courses %in% c("all", current_courses))) {
-    stop(paste0("Choose 'all' courses or provide a single character or vector ",
+    stop(paste0("Choose 'all' German courses or provide a single character or vector ",
       "with one of the following courses: ",
       "'DSB-01', 'DSB-02', 'DSB-03', 'DSB-04', 'DSB-05', 'DSB-06', 'Data analysis with R'. ",
       "For more info see the functions' documentation.")
@@ -48,7 +55,7 @@ install_dsb_courses <- function(courses = "all", force = FALSE) {
   }
   if (length(courses) == 1) {
     if (courses == "all") {
-      courses <- current_courses
+      courses <- current_courses[1:6]
     }
   }
   courses_swc <- c(
